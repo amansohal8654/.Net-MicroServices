c4f30936a0ded1e1b7a6a6ccef1c783faa1a6214
amansohal8654 <amansohal8654@gmail.com>
2021-10-21 15:55:13 +1300

    wired up with db context registration and add repo into the scop

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -7,9 +11,23 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
-        {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
 
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from Command Service");
+
+            var platformsItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformsItems));
         }
 
         [HttpPost]
@@ -20,6 +38,6 @@ namespace CommandsService.Controllers
             return Ok("Inbound test of from Platform Controler");
         }
 
-        
+
     }
 }
